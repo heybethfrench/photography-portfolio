780998c7b042bb7919a7c9175b1bbcf00c8a2749
heybethfrench <heybethfrench@gmail.com>
2019-06-26 18:48:04 -0500

    created new template for individual categories, and added links to the category images

--- src/main/java/com/bethfrench/photographyportfolio/controllers/CategoryController.java
+++ src/main/java/com/bethfrench/photographyportfolio/controllers/CategoryController.java
@@ -11,4 +11,49 @@ public class CategoryController {
         return "display-categories";
 
     }
+
+    @RequestMapping (value="categories/red")
+    public String displayCategoriesRed() {
+        return "category-page";
+    }
+
+    @RequestMapping (value="categories/black")
+    public String displayCategoriesBlack() {
+        return "category-page";
+    }
+
+    @RequestMapping (value="categories/blue")
+    public String displayCategoriesBlue() {
+        return "category-page";
+    }
+
+    @RequestMapping (value="categories/brown")
+    public String displayCategoriesBrown() {
+        return "category-page";
+    }
+
+    @RequestMapping (value="categories/green")
+    public String displayCategoriesGreen() {
+        return "category-page";
+    }
+
+    @RequestMapping (value="categories/orange")
+    public String displayCategoriesOrange() {
+        return "category-page";
+    }
+
+    @RequestMapping (value="categories/pink")
+    public String displayCategoriesPink() {
+        return "category-page";
+    }
+
+    @RequestMapping (value="categories/white")
+    public String displayCategoriesWhite() {
+        return "category-page";
+    }
+
+    @RequestMapping (value="categories/yellow")
+    public String displayCategoriesYellow() {
+        return "category-page";
+    }
 }
